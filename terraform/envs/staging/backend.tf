9a6b9c3b9f359e8507a977a78f49a67a13c409fc
Arnaud YANKWA WANDJI <yankwarmand@gmail.com>
2026-01-27 11:25:14 +0100

    cluster: update terraform backends to s3

--- terraform/envs/staging/backend.tf
+++ terraform/envs/staging/backend.tf
@@ -2,13 +2,13 @@ terraform {
   # Uncomment after running terraform in global/ first to create the S3 bucket
   # Then run: terraform init -migrate-state
 
-  # backend "s3" {
-  #   bucket         = "nzambe-terraform-state"
-  #   key            = "staging/terraform.tfstate"
-  #   region         = "us-west-2"
-  #   dynamodb_table = "nzambe-terraform-locks"
-  #   encrypt        = true
-  # }
+  backend "s3" {
+    bucket         = "nzambe-terraform-state"
+    key            = "staging/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "nzambe-terraform-locks"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
