a65ae4e5c15d153ab649f9e23aec9849244ee196
Arnaud YANKWA WANDJI <yankwarmand@gmail.com>
2026-02-03 14:00:44 +0100

    fix: fix s3vectors permissions

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,9 @@ from llama_index.embeddings.ollama import OllamaEmbedding
 # Set the environment variable to 'test' before any other imports happen.
 # This ensures that when your config.py is imported, it sees NZAMBE_ENV=test
 os.environ["NZAMBE_ENV"] = "test"
+os.environ["AWS_DEFAULT_REGION"] = "test"
+os.environ["AWS_ACCESS_KEY_ID"] = "test"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
 
 
 def set_mock_embed_model():
